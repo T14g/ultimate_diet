80f980041a6391bcae5e7956f992ff45608dd8dc
Tiago <ottiagos@gmail.com>
2022-12-06 23:21:57 -0300

    first commit, cleaning up basic react env

---
+++ src/components/BigTitle/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const BigTitle = ({title}) => <h1>{title}</h1>;
+
+export default BigTitle;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BigTitle from './components/BigTitle';
+
+ReactDOM.render(
+  <>
+    <BigTitle title="Ultimate Diet"/>
+  </> 
+  ,
+  document.getElementById('ultimate-diet')
+);
