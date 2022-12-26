49f3018526465485099060786c4584c07ef2f62f
Tiago <ottiagos@gmail.com>
2022-12-25 23:48:50 -0300

    styled

--- src/components/NewDiet/NewDiet.styles.js
+++ src/components/NewDiet/NewDiet.styles.js
@@ -1,14 +1,18 @@
 import styled from "styled-components";
 
-const NewDietStyles = styled.div`
+const NewDietStyles = styled.div``;
 
-`;
+export const Label = styled.div``;
 
 export const DietRow = styled.div`
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 `;
 
-export const DietColumn = styled.div``;
+export const DietColumn = styled.div`
+  select {
+    display: block;
+  }
+`;
 
 export default NewDietStyles;
