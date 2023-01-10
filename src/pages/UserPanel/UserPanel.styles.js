083feae50fa6daf0503562d33919f326565cae09
Tiago <ottiagos@gmail.com>
2023-01-09 23:52:03 -0300

    user panel switching screen view

---
+++ src/pages/UserPanel/UserPanel.styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const UserPanelStyles = styled.div`
+    text-align: center;
+`;
+
+export default UserPanelStyles;
--- src/pages/UserPanel/index.js
+++ src/pages/UserPanel/index.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
 import NewDiet from "../../components/NewDiet";
 import DietList from "../../components/DietList";
+import Button from "../../components/Button";
+
+import UserPanelStyles from "./UserPanel.styles";
 
 const UserPanel = () => {
   const [diets, setDiets] = useState([]);
   const [update, setUpdate] = useState(true);
+  const [action, setAction] = useState("");
 
   useEffect(() => {
     if (update) {
@@ -21,12 +25,16 @@ const UserPanel = () => {
   }, [update]);
 
   return (
-    <>
-      <NewDiet />
-      {diets.length > 0 && (
+    <UserPanelStyles>
+      <h2>Escolha uma ação</h2>
+      <p>Current action: {action}</p>
+      <Button text="Nova Dieta" handleOnClick={() => setAction("new-diet")} />
+      <Button text="Ver Dietas" handleOnClick={() => setAction("view-diets")} />
+      {action === "new-diet" && <NewDiet />}
+      {action === "view-diets" && diets.length > 0 && (
         <DietList list={diets} setUpdate={() => setUpdate(true)} />
       )}
-    </>
+    </UserPanelStyles>
   );
 };
 
