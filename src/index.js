d5ede349e1e52c4079f04ee212958ef6fb075d55
Tiago <ottiagos@gmail.com>
2023-01-19 23:41:52 -0300

    fixing body css with inline css

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import UserPanel from "./pages/UserPanel";
 
+import './styles.css';
+
 ReactDOM.render(<UserPanel />, document.getElementById("ultimate-diet"));
